72d76fa5031d948f273d6d95659d16aab7079e54
Deepak1549 <86293680+Deepak1549@users.noreply.github.com>
2024-06-05 13:32:25 +0530

    backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-backend-for-revhire-infrastructure-creation-1"
+    bucket         = "s3-backend-for-revhire-infrastructure-creation-p2-1"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "revhire-eks-backend-table"
+    dynamodb_table = "revhire-eks-backend-table-p2-1"
   }
 }
 
